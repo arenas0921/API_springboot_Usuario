a807fa96f666527bdb8b409a2f1d3e66dc00f578
arenas0921 <arenas09216671@gmail.com>
2023-10-01 17:10:43 -0500

    commitInitial

---
+++ src/main/java/com/roland/apirest/Person/PersonService.java
@@ -0,0 +1,16 @@
+package com.roland.apirest.Person;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PersonService {
+    
+    private final PersonRepository personRepo;
+
+    public void createPersona(Person person){
+        personRepo.save(person);
+    }
+}
